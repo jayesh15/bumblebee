9b2d139dda4b076d48606d86780a1dad578bf843
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-06-27 20:29:45 +0530

    Benefits

---
+++ client/src/components/home/Benefits.jsx
@@ -0,0 +1,53 @@
+import {LiaChalkboardTeacherSolid} from 'react-icons/lia'
+const Benefits = () => {
+    return (
+        <div className="mt-8 flex flex-col w-full items-center">
+            <h1 className="font-bold text-3xl lg:text-5xl">Benefits</h1>
+            <div className="mt-8 p-4 grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 w-full max-w-[1240px]">
+                <div className="flex p-2 border-[1px] border-gray-300 rounded-lg gap-4 w-full items-center">
+                    <div className=' bg-purple-300 p-6 rounded-full'>
+                        <LiaChalkboardTeacherSolid className=' text-purple-700 text-[30px]'/>
+                    </div>
+                    <div className="flex flex-col gap-2 items-start justify-between">
+                        <h1 className=' text-lg font-semibold text-blue-800'>Professional Teachers</h1>
+                        <p className=' text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus vitae, consequatur veritatis eligendi excepturi nostrum amet eum porro ab id?</p>
+
+                    </div>
+                </div>
+                <div className="flex p-2 border-[1px] border-gray-300 rounded-lg gap-4 w-full items-center">
+                    <div className=' bg-purple-300 p-6 rounded-full'>
+                        <LiaChalkboardTeacherSolid className=' text-purple-700 text-[30px]'/>
+                    </div>
+                    <div className="flex flex-col gap-2 items-start justify-between">
+                        <h1 className=' text-lg font-semibold text-blue-800'>Professional Teachers</h1>
+                        <p className=' text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus vitae, consequatur veritatis eligendi excepturi nostrum amet eum porro ab id?</p>
+
+                    </div>
+                </div>
+                <div className="flex p-2 border-[1px] border-gray-300 rounded-lg gap-4 w-full items-center">
+                    <div className=' bg-purple-300 p-6 rounded-full'>
+                        <LiaChalkboardTeacherSolid className=' text-purple-700 text-[30px]'/>
+                    </div>
+                    <div className="flex flex-col gap-2 items-start justify-between">
+                        <h1 className=' text-lg font-semibold text-blue-800'>Professional Teachers</h1>
+                        <p className=' text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus vitae, consequatur veritatis eligendi excepturi nostrum amet eum porro ab id?</p>
+
+                    </div>
+                </div>
+                <div className="flex p-2 border-[1px] border-gray-300 rounded-lg gap-4 w-full items-center">
+                    <div className=' bg-purple-300 p-6 rounded-full'>
+                        <LiaChalkboardTeacherSolid className=' text-purple-700 text-[30px]'/>
+                    </div>
+                    <div className="flex flex-col gap-2 items-start justify-between">
+                        <h1 className=' text-lg font-semibold text-blue-800'>Professional Teachers</h1>
+                        <p className=' text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus vitae, consequatur veritatis eligendi excepturi nostrum amet eum porro ab id?</p>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Benefits
--- client/src/pages/home/Content.jsx
+++ client/src/pages/home/Content.jsx
@@ -1,11 +1,13 @@
 import About from "../../components/home/About"
+import Benefits from "../../components/home/Benefits"
 import Hero from "../../components/home/Hero"
 
 const Content = () => {
   return (
-    <div className="flex flex-col w-full h-auto">
+    <div className="flex flex-col w-full gap-10 md:gap-20 h-auto">
         <Hero/>
         <About/>
+        <Benefits/>
     </div>
   )
 }
