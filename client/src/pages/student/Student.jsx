c0edcd4e0c32d724c68a802d410821871b4a06e1
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-06-28 17:56:08 +0530

    Added StudentProfile

--- client/src/pages/student/Student.jsx
+++ client/src/pages/student/Student.jsx
@@ -2,12 +2,16 @@ import { Outlet } from "react-router-dom"
 import StudentSidebar from "../../components/student/StudentSidebar"
 import { useEffect, useState } from "react"
 import MobileNavbar from "../../components/student/MobileNavbar"
+import StudentProfile from "../../components/student/StudentProfile"
 
 const Student = () => {
   const [isMobile, setIsMobile] = useState(false)
+  const [isProfile, setIsProfile] = useState(0)
   useEffect(() => {
     const handleResize = () => {
+      
       setIsMobile(window.innerWidth < 768);
+      setIsProfile(window.innerWidth > 1300)
     };
     handleResize();
     window.addEventListener('resize', handleResize);
@@ -24,6 +28,10 @@ const Student = () => {
       <div className=" flex flex-1">
         <Outlet />
       </div>
+      {
+        isProfile && <StudentProfile/>
+      }
+    
 
     </div>
   )
