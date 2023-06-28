2c9f32d545b3be822c8369723d0c2657e3822496
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-06-28 13:12:01 +0530

    Added colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#fdfdfd',
+        secondary:'#3d72de',
+      },
+    },
   },
   plugins: [],
 }
