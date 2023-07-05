47f29d99b2eb766151da6693a1d668e88a295d92
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-05 16:26:20 +0530

    user registration success

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,23 @@
-export const register = async(req,res,next) =>{
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import createError from "../utils/createError.js";
+export const register = async (req, res, next) => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    const hash = bcrypt.hashSync(req.body.password, 5);
+    const newUser = User({
+      ...req.body,
+      password: hash,
+    });
+    await newUser.save();
 
-}
+    res.status(201).send("User has been Created");
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const login = async(req,res,next) =>{
-    
-}
+export const login = async (req, res, next) => {};
 
-export const logout = async(req,res,next) =>{
-    
-}
+export const logout = async (req, res, next) => {};
