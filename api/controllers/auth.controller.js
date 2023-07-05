b7909c01210f88ab5aa862463ce958307b2507fe
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-05 16:13:05 +0530

    basic auth api setup

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = async(req,res,next) =>{
+
+}
+
+export const login = async(req,res,next) =>{
+    
+}
+
+export const logout = async(req,res,next) =>{
+    
+}
--- api/server.js
+++ api/server.js
@@ -1,6 +1,20 @@
 import cookieParser from 'cookie-parser'
 import express from 'express'
-
+import authRoute from "./routes/auth.route.js";
 const app = express()
 
-app.use(express.json()
+app.use(express.json())
+app.use(cookieParser())
+
+
+app.use('/api/auth',authRoute)
+
+app.use((err,req,res,next)=>{
+    const errStatus = err.status || 500
+    const errMessage = err.message || "Something went Wrong"
+    return res.status(errStatus).send(errMessage)
+})
+
+app.listen(8800,()=>{
+    console.log("Backend is Running")
+})
