aa533e498b2a0c7ca7b4e030cbec726dad3c868c
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-05 16:57:05 +0530

    Role check

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -22,11 +22,18 @@ export const register = async (req, res, next) => {
 export const login = async (req, res, next) => {
   try {
     await mongoose.connect(process.env.MONGO);
-    const user = await User.findOne({ username: req.body.username });
-    if (!user) return next(createError(404, "User not Found"));
+    const { username, password, role } = req.body;
 
-    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
-    if (!isCorrect) return next(createError(400, "Wrong Credentials"));
+    const user = await User.findOne({ username });
+    if (!user) return next(createError(404, "User not found"));
+
+    if (!role) return next(createError(400, "Role not specified"));
+    
+    const isCorrect = bcrypt.compareSync(password, user.password);
+    if (!isCorrect) return next(createError(400, "Wrong credentials"));
+
+    if (user.role !== role)
+      return next(createError(400, "Invalid role for this user"));
 
     const token = jwt.sign(
       {
@@ -36,12 +43,16 @@ export const login = async (req, res, next) => {
       process.env.JWT_KEY
     );
 
-    const { password, ...info } = user._doc;
-    res.cookie("accessToken", token, { httpOnly: true }).status(200).send(info);
+    const { password: _, ...info } = user._doc;
+    res
+      .cookie("accessToken", token, { httpOnly: true })
+      .status(200)
+      .send(info);
   } catch (error) {
-    next(err);
+    next(error);
   }
 };
+
 
 export const logout = async (req, res) => {
     res.clearCookie("accessToken", {
