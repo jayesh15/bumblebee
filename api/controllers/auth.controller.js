f7ee9421bcc023033aed784ff3e9e320fea9b034
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-05 16:32:51 +0530

    Logged Out

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -43,4 +43,9 @@ export const login = async (req, res, next) => {
   }
 };
 
-export const logout = async (req, res, next) => {};
+export const logout = async (req, res) => {
+    res.clearCookie("accessToken", {
+        sameSite: "none",
+        secure: true
+      }).status(200).send("User has been logged out");
+};
