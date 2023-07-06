2275e2b9ccbfd42baea8431671f1ce90d09ac146
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-06 14:13:08 +0530

    load Students

--- client/src/pages/faculty/ManageTasks.jsx
+++ client/src/pages/faculty/ManageTasks.jsx
@@ -1,4 +1,12 @@
+import { useQuery } from "@tanstack/react-query"
+import newRequest from "../../utils/newRequest"
+
 const ManageTasks = () => {
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ['students'],
+    queryFn: () => newRequest.get('students').then((res) => res.data)
+  })
+  console.log(data)
   return (
     <div>ManageTasks</div>
   )
