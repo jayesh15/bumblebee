c2b683da20cdc210a86a8608afcdd22d0888ac48
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-06 15:02:56 +0530

    load Students

--- client/src/pages/faculty/ManageTasks.jsx
+++ client/src/pages/faculty/ManageTasks.jsx
@@ -8,7 +8,32 @@ const ManageTasks = () => {
   })
   console.log(data)
   return (
-    <div>ManageTasks</div>
+    <div className="p-4 flex flex-col border-2 items-center justify-start w-full h-full">
+      <div className="flex flex-col gap-4 items-center w-full h-auto">
+        <h1 className=" font-semibold text-lg">Assign Tasks to Students</h1>
+        <div className="flex flex-col gap-2 items-center w-full">
+          {
+            isLoading ? "Loading Students" :
+              isError ? "Something Went Wrong" :
+                (
+                  data.map((student) => (
+                    
+                      <div key={student._id} className="flex w-full justify-between items-center p-4 border-[1px] border-gray-300">
+                        <div className="flex gap-4 items-center">
+                          <img className="w-14 h-14 object-cover object-center" src={student?.img || "/assets/noProfile.png"} alt="Student" />
+                          <div className="flex flex-col overflow-x-auto">
+                            <h1 className=" font-semibold">{student.username}</h1>
+                            <p className=" text-sm">{student.email}</p>
+                          </div>
+                        </div>
+                      </div>
+                    
+                  ))
+                )
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
