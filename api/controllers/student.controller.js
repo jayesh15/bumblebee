d430f46b0b7607c7fd2f6438403b52a6a52d80bc
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-06 14:29:19 +0530

    excluded password

--- api/controllers/student.controller.js
+++ api/controllers/student.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js'
 export const getStudents = async(req, res, next)=>{
     try {
-        const students= await User.find({ role: "student" })
+        const students= await User.find({ role: "student" }).select("-password")
         res.status(200).send(students)
     } catch (error) {
         next(error)
