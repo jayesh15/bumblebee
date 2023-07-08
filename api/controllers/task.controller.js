9a3c9dac2e52b297804d6c87d95a8244e48f3e09
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-09 01:14:32 +0530

    Addtask part1

--- api/controllers/task.controller.js
+++ api/controllers/task.controller.js
@@ -1,3 +1,29 @@
-export const addTask = async(req, res,next)=>{
-    res.send("Ok")
-}
+import Task from '../models/task.model.js';
+import User from '../models/user.model.js';
+import createError from '../utils/createError.js';
+
+export const addTask = async (req, res, next) => {
+  try {
+    const { title, description, assignedTo, deadline } = req.body;
+    const createdBy = req.userId;
+
+    const createdByUser = await User.findById(createdBy);
+    if (createdByUser.role !== 'teacher') {
+      return next(createError(403, 'Only teachers can assign tasks.'));
+    }
+
+    const newTask = new Task({
+      title,
+      description,
+      assignedTo,
+      deadline,
+      createdBy,
+    });
+
+    await newTask.save();
+
+    res.status(201).send('Task added successfully.');
+  } catch (error) {
+    next(error);
+  }
+};
