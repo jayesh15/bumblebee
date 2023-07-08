3805c04802792914810f0db7513049cc4d51d237
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-09 03:03:54 +0530

    Icons corrected

--- client/src/pages/faculty/FacultyDashboard.jsx
+++ client/src/pages/faculty/FacultyDashboard.jsx
@@ -1,5 +1,6 @@
 import { FaChildren } from 'react-icons/fa6'
 import { BiTask } from 'react-icons/bi'
+import {MdPendingActions,MdAssignment} from 'react-icons/md'
 import { useQuery } from '@tanstack/react-query'
 import newRequest from '../../utils/newRequest'
 const FacultyDashboard = () => {
@@ -42,7 +43,7 @@ const FacultyDashboard = () => {
       <div className="grid w-full grid-cols-1 md:grid-cols-2 gap-4">
         <div className="flex py-8 px-4  justify-center items-center gap-4 w-full  rounded-lg  bg-gradient-to-br from-blue-300 to-blue-500">
           <div className=' p-4 bg-gradient-to-br from-blue-600 to-blue-800 rounded-2xl'>
-            <FaChildren className=' text-[25px]' />
+            <FaChildren className='text-white text-[25px]' />
           </div>
           <div className='flex gap-2 items-baseline '>
             <p className=" font-medium text-yellow-300 text-lg">No. of Students</p>
@@ -57,7 +58,7 @@ const FacultyDashboard = () => {
         </div>
         <div className="flex py-8 px-4 justify-center items-center gap-4 w-full  rounded-lg bg-gradient-to-br from-blue-300 to-blue-500">
           <div className=' p-4 bg-gradient-to-br from-blue-600 to-blue-800 rounded-2xl'>
-            <BiTask className=' text-[25px]' />
+            <MdAssignment className=' text-white text-[25px]' />
           </div>
           <div className='flex gap-2 items-baseline '>
             <p className=" font-medium text-yellow-300 text-lg">Tasks Assigned</p>
@@ -66,7 +67,7 @@ const FacultyDashboard = () => {
         </div>
         <div className="flex py-8 px-4  justify-center items-center gap-4 w-full  rounded-lg  bg-gradient-to-br from-blue-300 to-blue-500">
           <div className=' p-4 bg-gradient-to-br from-blue-600 to-blue-800 rounded-2xl'>
-            <FaChildren className=' text-[25px]' />
+            <BiTask className=' text-white text-[25px]' />
           </div>
           <div className='flex gap-2 items-baseline '>
             <p className=" font-medium text-yellow-300 text-lg">Tasks Completed</p>
@@ -75,7 +76,7 @@ const FacultyDashboard = () => {
         </div>
         <div className="flex py-8 px-4 justify-center items-center gap-4 w-full  rounded-lg bg-gradient-to-br from-blue-300 to-blue-500">
           <div className=' p-4 bg-gradient-to-br from-blue-600 to-blue-800 rounded-2xl'>
-            <BiTask className=' text-[25px]' />
+            <MdPendingActions className='text-white text-[25px]' />
           </div>
           <div className='flex gap-2 items-baseline '>
             <p className=" font-medium text-yellow-300 text-lg">Tasks Pending</p>
