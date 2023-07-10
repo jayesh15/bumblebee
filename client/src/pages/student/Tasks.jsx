a1f2d33c7d0cbbdc9da537c110c86812185ae83c
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-10 20:39:28 +0530

    Mark Done Success

--- client/src/pages/student/Tasks.jsx
+++ client/src/pages/student/Tasks.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query"
 import newRequest from "../../utils/newRequest"
+import { useState } from "react"
 
 const Tasks = () => {
   const user = JSON.parse(localStorage.getItem("currentUser"))
@@ -9,6 +10,19 @@ const Tasks = () => {
   })
 
   const assignedTasks = tasks && tasks.filter(task => task.assignedTo.studentId === user._id);
+
+  
+  
+  const handleDoneTask = (taskId) =>{
+      
+      
+      try {
+        newRequest.patch(`tasks/${taskId}`)
+        alert("Task Marked as Done")
+      } catch (error) {
+        alert(error)
+      }
+  }
 
   return (
     <div className="  flex flex-col p-4 gap-4 w-full items-center">
@@ -29,7 +43,7 @@ const Tasks = () => {
                 <div className="flex w-full">
                 <p className=" font-semibold">Due date: <span className=" font-semibold text-sm text-gray-600">{new Date(task.deadline).toLocaleDateString('en-GB')}</span></p>
                 </div>
-                <button className=" w-full max-w-[100px] px-4 py-2 bg-blue-500 rounded-md font-semibold text-white hover:opacity-70 active:opacity-30 ease-in-out duration-150 transition-all">Done</button>
+                <button onClick={()=>handleDoneTask(task._id)} className=" w-full max-w-[100px] px-4 py-2 bg-blue-500 rounded-md font-semibold text-white hover:opacity-70 active:opacity-30 ease-in-out duration-150 transition-all">Done</button>
               </div>
             </>
           ))
