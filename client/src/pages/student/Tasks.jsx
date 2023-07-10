57283fcc782c96e7c64dae3619bdac2d8876c3ac
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-10 20:13:32 +0530

    Render Tasks

--- client/src/pages/student/Tasks.jsx
+++ client/src/pages/student/Tasks.jsx
@@ -11,17 +11,32 @@ const Tasks = () => {
   const assignedTasks = tasks && tasks.filter(task => task.assignedTo.studentId === user._id);
 
   return (
-    <div className="  flex w-full">{
-      isloadingTasks ? "..." : isErrorTasks ? "Error" : (
-        assignedTasks.map((task) => (
-          <>
-            <div>
-              <h1>{task.title}</h1>
-            </div>
-          </>
-        ))
-      )
-    }</div>
+    <div className="  flex flex-col p-4 gap-4 w-full items-center">
+
+      {
+        isloadingTasks ? "Loading tasks..." : isErrorTasks ? "Something went Wrong" : (
+          assignedTasks.map((task) => (
+            <>
+              <div className="flex flex-col items-center gap-2 w-full px-2 md:px-4 py-4 border-[1px] border-gray-300 rounded-lg">
+                <div className="flex gap-2 flex-wrap w-full justify-between items-baseline">
+                  <p className=" font-bold text-xl ">{task.title}</p>
+                  <p className=" font-semibold">Assinged: <span className=" font-semibold text-sm text-gray-600">{new Date(task.createdAt).toLocaleDateString('en-GB')}</span></p>
+                </div>
+                <div>
+                  <p className=" font-bold ">Description</p>
+                  <p className=" text-justify">{task.description} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam veritatis optio praesentium temporibus laboriosam facere? Omnis accusamus fugiat inventore corporis.</p>
+                </div>
+                <div className="flex w-full">
+                <p className=" font-semibold">Due date: <span className=" font-semibold text-sm text-gray-600">{new Date(task.deadline).toLocaleDateString('en-GB')}</span></p>
+                </div>
+                <button className=" w-full max-w-[100px] px-4 py-2 bg-blue-500 rounded-md font-semibold text-white hover:opacity-70 active:opacity-30 ease-in-out duration-150 transition-all">Done</button>
+              </div>
+            </>
+          ))
+        )
+      }
+
+    </div>
   )
 }
 
