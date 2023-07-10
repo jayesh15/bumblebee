f1b70fe445c4c40c567350d207b378e89137d0ae
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-10 19:49:40 +0530

    Filter Tasks

--- client/src/pages/student/Tasks.jsx
+++ client/src/pages/student/Tasks.jsx
@@ -1,6 +1,27 @@
+import { useQuery } from "@tanstack/react-query"
+import newRequest from "../../utils/newRequest"
+
 const Tasks = () => {
+  const user = JSON.parse(localStorage.getItem("currentUser"))
+  const { isLoading: isloadingTasks, isError: isErrorTasks, data: tasks } = useQuery({
+    queryKey: ['tasks'],
+    queryFn: () => newRequest.get('tasks').then((res) => res.data)
+  })
+
+  const assignedTasks = tasks && tasks.filter(task => task.assignedTo.studentId === user._id);
+
   return (
-    <div className="  flex w-full">Tasks</div>
+    <div className="  flex w-full">{
+      isloadingTasks ? "..." : isErrorTasks ? "Error" : (
+        assignedTasks.map((task) => (
+          <>
+            <div>
+              <h1>{task.title}</h1>
+            </div>
+          </>
+        ))
+      )
+    }</div>
   )
 }
 
